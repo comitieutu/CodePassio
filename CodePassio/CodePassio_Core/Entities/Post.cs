c6fe2daef4d3ac8eab9c4bbbebe66e0b36ff16bc
Tran Thanh <tranthanhdeveloper@gmail.com>
2019-08-12 21:22:43 +0700

    Update Post: add more properties

--- CodePassio/CodePassio_Core/Entities/Post.cs
+++ CodePassio/CodePassio_Core/Entities/Post.cs
@@ -7,12 +7,21 @@ namespace CodePassio_Core.Entities
     public class Post : BaseEntity
     {
         public string Title { get; set; }
+
+        public string Excerpt { get; set; }
+
+        public byte Status { get; set; }
+
         public string Content { get; set; }
+
         public string Tag { get; set; }
+
         public Guid CategoryId { get; set; }
 
         public virtual Category Category { get; set; }
+
         public ICollection<Comment> Comments { get; set; }
+
         public List<PostTag> PostTags { get; set; }
     }
 }
