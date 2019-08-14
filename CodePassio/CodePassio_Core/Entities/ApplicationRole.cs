387e9900701300e7079129241b90dd5f7e92ab97
comitieutu <lecomineko@gmail.com>
2019-08-14 23:10:34 +0700

    AddArea

---
+++ CodePassio/CodePassio_Core/Entities/ApplicationRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace CodePassio_Core.Entities
+{
+    public class ApplicationRole : IdentityRole
+    {
+
+        public ApplicationRole() : base() { }
+
+        public ApplicationRole(string roleName) : base(roleName) { }
+
+        public ApplicationRole(string roleName, string description) : base(roleName)
+        {
+            base.Name = roleName;
+            this.Description = description;
+        }
+
+        public string Description { get; set; }
+        public virtual ICollection<ApplicationUserRole> UserRoles { get; set; }
+    }
+    public enum Role
+    {
+        SuperAdmin = 1,
+        Manager = 2,
+        User = 3
+    }
+}
--- CodePassio/CodePassio_Web/Migrations/20190813164059_AddIdentity.Designer.cs
+++ CodePassio/CodePassio_Web/Migrations/20190813164059_AddIdentity.Designer.cs
@@ -4,14 +4,16 @@ using CodePassio_Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CodePassio_Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190813164059_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
