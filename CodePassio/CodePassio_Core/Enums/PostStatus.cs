ae72087bca7d4b4f1ef3f8e7efda83665326fa88
Tran Thanh <tranthanhdeveloper@gmail.com>
2019-09-01 15:59:26 +0700

    Add post status

---
+++ CodePassio/CodePassio_Core/Enums/PostStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodePassio_Core.Enums
+{
+    public enum PostStatus
+    {
+        Publish = 0,
+        Future = 1,
+        Draft = 2,
+        Pending = 3,
+        Private = 4,
+        Trash = 5,
+        AutoDraft = 6
+    }
+}
